dc569311b6ab30d1d7d98a5c700df1babb4c73e9
glebbykov <glebbykov1993@gmail.com>
2023-01-24 06:01:12 +0000

    add selection of different os

--- pass_on.sh
+++ pass_on.sh
@@ -11,9 +11,19 @@ read -s user_password
 
 echo $user_password | sudo passwd --stdin $(whoami)
 
-sudo systemctl reload ssh
+# Determine the system type
+if [ -f /etc/lsb-release ]; then
+    # Debian/Ubuntu Linux user run
+    sudo systemctl reload ssh.service
+elif [ -f /etc/redhat-release ]; then
+    # RHEL/CentOS Linux user run
+    sudo systemctl reload sshd.service
+else
+    echo "Unable to determine system type"
+    exit 1
+fi
 
-if grep -q "^PasswordAuthentication yes" /etc/ssh/sshd_config; then
+if sudo grep -q "^PasswordAuthentication yes" /etc/ssh/sshd_config; then
     echo "Password authentication is now enabled"
 else
     echo "Error enabling password authentication"
