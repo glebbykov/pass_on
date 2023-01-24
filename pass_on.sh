a676abd82d59b0c2e55d4a4c6392e3f507b8bb20
glebbykov <122178014+glebbykov@users.noreply.github.com>
2023-01-24 20:09:46 +0300

    add case OS

--- pass_on.sh
+++ pass_on.sh
@@ -11,20 +11,34 @@ read -s user_password
 
 echo $user_password | sudo passwd $(whoami)
 
-# Determine the system type
-if [ -f /etc/lsb-release ]; then
-    # Ubuntu Linux user run
-    sudo systemctl reload ssh.service
+# Identify the Linux platform
+platform=$(cat /etc/*-release | grep -E '^ID=' | cut -d '=' -f 2 | tr -d '"')
 
-elif [ -f /etc/os-release ]; then
-    # Debian Linux user run
-    sudo systemctl reload ssh.service
+case "$platform" in
+    centos)
+      sudo systemctl reload sshd.service
+      ;;
 
-elif [ -f /etc/redhat-release ]; then
-    # RHEL/CentOS Linux user run
-    sudo systemctl reload sshd.service
-else
-    echo "Unable to determine system type"
+    debian)
+      sudo systemctl reload ssh.service
+      ;;
+
+    fedora)
+      sudo systemctl reload sshd.service
+      ;;
+
+    ubuntu)
+      sudo systemctl reload ssh.service
+      ;;
+    *)
+      # Error message
+      echo "Error: Unable to determine system type"
+      exit 1
+      ;;
+  esac
+
+if [ $? -ne 0 ]; then
+    echo "Error reloading the ssh service. Exiting."
     exit 1
 fi
 
@@ -32,4 +46,5 @@ if sudo grep -q "^PasswordAuthentication yes" /etc/ssh/sshd_config; then
     echo "Password authentication is now enabled"
 else
     echo "Error enabling password authentication"
+    exit 1
 fi
